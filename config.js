b57e43ca19216f632352aa84a78240eea67b001a
Akash Krishnan <ak@aakay.net>
2014-05-19 15:11:55 -0700

    Added config

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  log: {
+    file: {
+      express: 'express.log'
+    }
+  },
+  web: {
+    name: 'Occupational Octaves Piano',
+    port: 8080,
+    domain: 'localhost:8080',
+    secret: 'secret'
+  }
+};
